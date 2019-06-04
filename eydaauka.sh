bd4b743ae15d6142ff13d0a5dd541e2cdd7baeb4
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2019-06-04 13:24:14 +0000

    Delete eydaauka.sh

--- eydaauka.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Eyða auðri línu
-sed -i '/^$/d' ../../icecorpus/finished/*.psd
-#Eyða línu sem inniheldur '(ID'
-sed -i '/(ID/d' ../../icecorpus/finished/*.psd
-#Eyða öllum '( '
-sed -i 's/( //g' ../../icecorpus/finished/*.psd
