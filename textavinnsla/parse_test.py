d1b10a04a43d7fbb57af8d3e14e1b3d01c8b88fe
hinrikur <hih43@hi.is>
2019-06-04 16:35:32 +0000

    update parse_test.py

--- textavinnsla/parse_test.py
+++ textavinnsla/parse_test.py
@@ -2,6 +2,7 @@ from nltk.corpus.util import LazyCorpusLoader
 from nltk.corpus.reader import CategorizedBracketParseCorpusReader
 from nltk.data import path
 from nltk.tree import *
+import string
 
 # path.extend(['/Users/hinrik/Documents/trjabankar'])
 # path.extend(['./icepahc_nltk/'])
@@ -18,29 +19,70 @@ icepahc = LazyCorpusLoader(
     r'.*\.psd', cat_pattern=r'.*(nar|rel|sci|bio|law)\-.*'
 )
 
-tree = icepahc.parsed_sents()[5]
+tree = icepahc.parsed_sents()[6]
 
 ptree = ParentedTree.convert(tree)
 
-sentence = []
-runner = 0
-sentence.append(['nr.', 'token', 'lemma', 'UD_tag', 'ice_tag', 'feats', 'rel', 'rel_type'])
-for i in tree.pos():
-    word = i[0].split('-')
-    token = word[0]
-    if token[0] in ['*', '0']: continue
-    lemma = word[1]
-    ice_tag = i[1]
-    runner += 1
-    UD_tag = '_'
-    feats = '_'
-    rel = '_'
-    rel_type = '_'
-    line = [str(runner), token, lemma, UD_tag, ice_tag, feats, rel, rel_type]
-    sentence.append(line)
+cconj = {'og', 'eða', 'en', 'heldur', 'enda', 'ellegar',
+        'bæði','hvorki','annaðhvort','hvort', 'ýmist'}
+
+tags = {
+    # ipsd_tag : UD_tag
+    'N' : 'NOUN',   # generalized nouns tagged as NOUN
+    'D' : 'DET',    # generalized determiners tagged as DET
+    'P' : 'ADP',    # generalized prepositions tagged as ADP
+    'Q' : 'ADJ',    # quantifiers tagged as ADJ - ATH ÞETTA ÞARF AÐ ENDURSKOÐA
+    'NPR' : 'POPN', # proper nouns tagged as POPN
+    'ADJ' : 'ADJ',
+    'PRO' : 'PRON',
+    'NUM' : 'NUM',
+    'ADV' : 'ADV',
+    'NEG' : 'ADV',
+    'ALSO' : 'ADV'
+}
+
+def get_UD_tag(tag, word):
+    # if ipsd_tag.beginswith('NPR'):
+    #     return tags['NPR']
+    try:
+        return tags[tag]
+    except:
+        if tag == 'NEG':
+            return tags[tag]
+        elif tag.startswith('PRO'):
+            return tags['PRO']
+        elif tag == 'CONJ' and word in cconj:
+            return 'CCONJ'
+        elif tag in string.punctuation:
+            return 'PUNCT'
+        else:
+            try:
+                return tags[tag[0]]
+            except:
+                return '_'
+
+def word_info(sentence):
+    sentence = []
+    runner = 0
+    sentence.append(['nr.', 'token', 'lemma', 'UD_tag', 'ice_tag', 'feats', 'rel', 'rel_type'])
+    for leaf in tree.pos():
+        word = leaf[0].split('-')
+        token = word[0]
+        if token[0] in ['*', '0']: continue
+        lemma = word[1]
+        ipsd_tag = leaf[1]
+        runner += 1
+        UD_tag = get_UD_tag(ipsd_tag, lemma)
+        feats = '_'
+        rel = '_'
+        rel_type = '_'
+        line = [str(runner), token, lemma, UD_tag, ipsd_tag, feats, rel, rel_type]
+        sentence.append(line)
+    return sentence
+
 print()
-for i in sentence:
-    print('\t'.join(i))
+for word in word_info(tree):
+    print('\t'.join(word))
 
 
 # print(tree.pformat_latex_qtree())
