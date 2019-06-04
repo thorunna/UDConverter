a1102e6833db8a82ca0c31c11805149dbdfe33ee
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2019-06-04 13:31:36 +0000

    Update eydaauka.sh

--- textavinnsla/eydaauka.sh
+++ textavinnsla/eydaauka.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Eyða auðri línu
-sed -i '/^$/d' /icecorpus/finished/*.psd
+sed -i '/^$/d' /../icecorpus/finished/*.psd
 #Eyða línu sem inniheldur '(ID'
-sed -i '/(ID/d' /icecorpus/finished/*.psd
+sed -i '/(ID/d' /../icecorpus/finished/*.psd
 #Eyða öllum '( '
-sed -i 's/( //g' /icecorpus/finished/*.psd
+sed -i 's/( //g' /../icecorpus/finished/*.psd
