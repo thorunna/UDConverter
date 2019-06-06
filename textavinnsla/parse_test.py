1063714c10d0df12c9ceca147815849bdbd4d40d
hinrikur <hih43@hi.is>
2019-06-06 10:03:53 +0000

    early feature processing added

--- textavinnsla/parse_test.py
+++ textavinnsla/parse_test.py
@@ -2,6 +2,7 @@ from nltk.corpus.util import LazyCorpusLoader
 from nltk.corpus.reader import CategorizedBracketParseCorpusReader
 from nltk.data import path
 from nltk.tree import *
+from tokenizer import correct_spaces
 from pprint import pprint
 import string
 import re
@@ -20,17 +21,6 @@ icepahc = LazyCorpusLoader(
     'icepahc-v0.9/psd_orig/', CategorizedBracketParseCorpusReader,
     r'.*\.psd', cat_pattern=r'.*(nar|rel|sci|bio|law)\-.*'
 )
-
-trees = icepahc.parsed_sents()[0:20]
-# tree = icepahc.parsed_sents()[0]
-# tree = trees[0]
-
-# pprint(tree.__dir__())
-# print(tree)
-# pprint(icepahc.fileids())
-
-# ptree = ParentedTree.convert(tree)
-
 cconj = {'og', 'eða', 'en', 'heldur', 'enda', 'ellegar',
         'bæði','hvorki','annaðhvort','hvort', 'ýmist'}
 
@@ -61,6 +51,38 @@ tags = {
     'OTHERS' : 'PRON'
 }
 
+features = {
+    'NOUN' : {
+        'Case' : {
+            'N' : 'Nom',    # nominative case
+            'A' : 'Acc',    # accusative case
+            'D' : 'Dat',    # dative case
+            'G' : 'Gen'     # genitive case
+        },
+        'Number': {
+            'NS' : 'Plur',   # plural number
+            'N' : 'Sing'    # singular number
+        },
+        'Definite' : { # ATH this may be dropped from feature dict
+            '' : 'Def',
+            '' : 'Ind'
+        }
+    },
+    'DET' : {},
+    'ADP' : {},
+    'ADJ' : {},
+    'SCONJ' : {},
+    'VERB' : {},
+    'DET' : {},
+    'PART' : {},
+    'POPN' : {},
+    'PRON' : {},
+    'NUM' : {},
+    'ADJ' : {},
+    'ADV' : {}
+}
+
+
 def get_UD_tag(tag, word):
     # if ipsd_tag.beginswith('NPR'):
     #     return tags['NPR']
@@ -84,30 +106,96 @@ def get_UD_tag(tag, word):
             except:
                 return '_'
 
-def word_info(sentence):
+def sent_text(sentence):
+    '''
+    Takes in a nltk Tree object and returns the sentence text in string form
+    '''
+    text = []
+    leaves = sentence.leaves()
+    for leaf in leaves:
+        leaf = leaf.split('-')
+        if leaf[0][0] in {'*', '0'}: continue
+        text.append(leaf[0])
+    text = '# text = ' + correct_spaces(' '.join(text))
+    return text
 
+def word_info(sentence):
+    '''
+    Takes in a nltk Tree object and returns an approximation of the tree sentence
+    in the CONLLU format for UD
+    '''
     sentence = []
     runner = 0
-    sentence.append(['nr.', 'token', 'lemma', 'UD_tag', 'ice_tag', 'feats', 'rel', 'rel_type'])
+    # print(tree.leaves())
+    sentence.append(sent_text(tree))
+    sentence.append(['ID', 'FORM', 'LEMMA', 'UPOS', 'XPOS', 'FEATS', 'HEAD', 'DEPREL', 'DEPS', 'MISC'])
     for leaf in tree.pos():
-        word = leaf[0].split('-')
-        token = word[0]
-        if token[0] in ['*', '0']: continue
-        lemma = word[1]
-        ipsd_tag = leaf[1]
-        runner += 1
-        UD_tag = get_UD_tag(ipsd_tag, lemma)
-        feats = '_'
-        rel = '_'
-        rel_type = '_'
-        line = [str(runner), token, lemma, UD_tag, ipsd_tag, feats, rel, rel_type]
+        # print(leaf)
+        runner += 1 # runner updated for counting
+        ID = str(runner) # ID: Word index, integer starting at 1 for each new sentence.
+        word = leaf[0].split('-') # token and lemma seperated.
+        FORM = word[0] # FORM: Word form or punctuation symbol (token).
+        if FORM[0] in ['*', '0']: continue
+        LEMMA = word[1] # LEMMA: Lemma or stem of word form.
+        XPOS = leaf[1] # XPOS: Language-specific part-of-speech tag (IcePaHC)
+        UPOS = get_UD_tag(XPOS, LEMMA) # UPOS: Universal part-of-speech tag.
+        FEATS = '_' # FEATS: List of morphological features from the universal feature inventory
+        HEAD = '_' # HEAD: Head of the current word, which is either a value of ID or zero (0).
+        DEPREL = '_' # DEPREL: Universal dependency relation to the HEAD (root iff HEAD = 0)
+        DEPS = '_' # DEPS: Enhanced dependency graph in the form of a list of head-deprel pairs.
+        MISC = '_' # MISC: Any other annotation.
+        line = [str(runner), FORM, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC]
         sentence.append(line)
     return sentence
 
-for tree in trees:
-    print()
-    for word in word_info(tree):
-        print('\t'.join(word))
+# ID: Word index, integer starting at 1 for each new sentence.
+# FORM: Word form or punctuation symbol.
+# LEMMA: Lemma or stem of word form.
+# UPOS: Universal part-of-speech tag.
+# XPOS: Language-specific part-of-speech tag; underscore if not available.
+# FEATS: List of morphological features from the universal feature inventory or from a defined language-specific extension; underscore if not available.
+# HEAD: Head of the current word, which is either a value of ID or zero (0).
+# DEPREL: Universal dependency relation to the HEAD (root iff HEAD = 0) or a defined language-specific subtype of one.
+# DEPS: Enhanced dependency graph in the form of a list of head-deprel pairs.
+# MISC: Any other annotation.
+
+fileids = icepahc.fileids()
+# trees = icepahc.parsed_sents()[0:10]
+
+# tree = trees[0]
+
+# for s in trees:
+#     print(s)
+
+current_sentence = 0
+
+with open('is_test.conllu', 'w') as file:
+    while current_sentence <= 100:
+        try:
+            # file.write('hello')
+            tree = icepahc.parsed_sents()[current_sentence]
+            print('\n# sent_id = ', current_sentence + 1)
+            # file.write('# sent_id = ' + str(current_sentence + 1))
+            # file.write('\n')
+            for line in word_info(tree):
+                if line[0] == '#':
+                    print(line)
+                    # file.write(line)
+                    # file.write('\n')
+                else:
+                    print('\t'.join(line))
+                    # file.write('\t'.join(line))
+                    # file.write('\n')
+            # file.write('\n')
+            current_sentence += 1
+        except:
+            # raise
+            print('\n# sent_id = ', current_sentence)
+            print('Failure')
+            current_sentence += 1
+            continue
+
+
 
 
 # print(tree.pformat_latex_qtree())
