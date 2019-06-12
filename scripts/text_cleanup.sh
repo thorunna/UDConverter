c3ce3c916578e54b11a678fbc89ce1af2d8ed66c
hinrikur <hih43@hi.is>
2019-06-12 13:45:19 +0000

    added rm-code.prl to text processing

--- scripts/text_cleanup.sh
+++ scripts/text_cleanup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# HH Path:
+# ./testing/corpora/icepahc-v0.9/psd/*.psd
+
+# ÞA Path:
+# ../icecorpus/finished/*.psd
+
+./scripts/rm-code.prl $1 > $2
+
 #Eyða (CODE...)
-sed -i 's/(CODE[ {}*<>a-zA-Z0-9ðÐÖá_:-?/]*)//g' ../icecorpus/finished/*.psd
+sed -i "" 's/(CODE[ {}*<>a-zA-Z0-9a-zA-ZþæðöÞÆÐÖáéýúíóÁÉÝÚÍÓ\.$_:-?/]*)//g' $2 #./testing/corpora/icepahc-v0.9/psd_orig/*.psd
 #Eyða auðri línu
-sed -i '/^$/d' ../icecorpus/finished/*.psd
+sed -i "" '/^$/d' $2 #./testing/corpora/icepahc-v0.9/psd_orig/*.psd
 #Eyða línu sem inniheldur '(ID'
-sed -i '/(ID/d' ../icecorpus/finished/*.psd
+sed -i "" '/(ID/d' $2 #./testing/corpora/icepahc-v0.9/psd_orig/*.psd
 #Eyða öllum '( '
-sed -i 's/( //g' ../icecorpus/finished/*.psd
+sed -i "" 's/( //g' $2 #./testing/corpora/icepahc-v0.9/psd_orig/*.psd
+
+# sed -i "" 's/) //g' ./testing/corpora/icepahc-v0.9/psd_orig/*.psd
