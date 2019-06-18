e2f98f46199da1202e3fb54711b6f744215e4196
hinrikur <hih43@hi.is>
2019-06-18 09:54:40 +0000

    fixed broken comments

--- scripts/text_cleanup.sh
+++ scripts/text_cleanup.sh
@@ -2,7 +2,8 @@
 
 '''
 12.06.19
-Hinrik Hafsteinsson & Þórunn Arnardóttir
+Hinrik Hafsteinsson
+Þórunn Arnardóttir
 
 Script for cleaning IcePaHC corpus files (.psd)
  - Removes sentence ID tags
@@ -38,21 +39,21 @@ done
 
 for file in $out_dir/*; do
   echo "Working on file: ${file##*/}"
-  perl ./rm-nonstruct.prl -i $file
+  perl scripts/rm-nonstruct.prl -i $file
   #Delete (CODE...)
-  sed -i 's/(CODE[ {}*<>a-zA-Z0-9a-zA-ZþæðöÞÆÐÖáéýúíóÁÉÝÚÍÓ\.$_:-?/]*)//g' $file 
+  sed -i "" 's/(CODE[ {}*<>a-zA-Z0-9a-zA-ZþæðöÞÆÐÖáéýúíóÁÉÝÚÍÓ\.$_:-?/]*)//g' $file
   #Delete (ID...))
   sed -i "" 's/(ID [0-9]*\.[A-Z]*\.[A-Z]*-[A-Z]*[,\.][0-9]*[,\.][0-9]*))//g' $file
   #Delete empty lines
-  sed -i '/^$/d' $file 
+  sed -i "" '/^$/d' $file
   #Delete lines which include (ID
-  #sed -i '/(ID/d' $file 
+  #sed -i '/(ID/d' $file
   #Delete lines which only include (. ?-?)) or (. .-.)) at the beginning of line
-  sed -i 's/^(\. [\.?]-[\.?]))$//g' $file
+  sed -i "" 's/^(\. [\.?]-[\.?]))$//g' $file
   #Delete every instance of '( '
-  sed -i 's/( //g' $file 
+  sed -i "" 's/( //g' $file
   #Delete last character in file (uneven parentheses)
-  sed -i '$ s/.$//' $file
+  sed -i "" '$ s/.$//' $file
 done
 
   # sed -i "" 's/) //g' #./testing/corpora/icepahc-v0.9/psd_orig/*.psd
