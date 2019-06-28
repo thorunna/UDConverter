dbff7d1bbb83d9593a22f7bb0d40e416b99bd516
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2019-06-28 11:13:55 +0000

    added head rules

--- scripts/convert.py
+++ scripts/convert.py
@@ -28,19 +28,66 @@ class Converter():
         self.t = None
         self.dg = None
         self.head_rules = {
-            'IP-MAT'  : {'dir':'r', 'rules':['VB.*','RD.*','BE.*','N.*']},
-            'IP-SUB'  : {'dir':'r', 'rules':['BED.','.*']},
-            'NP-SBJ'  : {'dir':'r', 'rules':['N-N']},
-            'NP-OB1'  : {'dir':'r', 'rules':['N-A', 'NPR-A', 'ONE+Q-A']},
-            'NP'      : {'dir':'r', 'rules':['NS-.', 'N-.']},
-            'NP-MSR'  : {'dir':'r', 'rules':['NS-.', 'N-.']},
-            'ADJP'    : {'dir':'r', 'rules':['ADJ-N']},
-            'ADJP-SPR': {'dir':'r', 'rules':['ADJ-N']},
-            'PP'      : {'dir':'r', 'rules':['P']},
-            'CP-THT'  : {'dir':'r', 'rules':['IP-SUB','.*']},
+#            'IP-MAT'  : {'dir':'r', 'rules':['VB.*','RD.*','BE.*','N.*']},
+#            'IP-SUB'  : {'dir':'r', 'rules':['BED.','.*']},
+#            'NP-SBJ'  : {'dir':'r', 'rules':['N-N']},
+#            'NP-OB1'  : {'dir':'r', 'rules':['N-A', 'NPR-A', 'ONE+Q-A']},
+#            'NP'      : {'dir':'r', 'rules':['NS-.', 'N-.']},
+#            'NP-MSR'  : {'dir':'r', 'rules':['NS-.', 'N-.']},
+#            'ADJP'    : {'dir':'r', 'rules':['ADJ-N']},
+#            'ADJP-SPR': {'dir':'r', 'rules':['ADJ-N']},
+#            'PP'      : {'dir':'r', 'rules':['P']},
+#            'CP-THT'  : {'dir':'r', 'rules':['IP-SUB','.*']},
             #'IP-INF'  : {'dir':'r', 'rules':['IP-INF']},  # óþarfi, er fyrsti frá vinstri
             #'CONJP'   : {'dir':'r', 'rules':['IP-INF']}   # óþarfi, er fyrsti frá vinstri
-        }
+            
+            'IP-INF'        : {'dir':'r', 'rules':['VB']},
+            'IP-MAT'        : {'dir':'r', 'rules':['VB.*','RD.*','BE.*', 'DO.*','N.*']}, 
+            'IP-MAT-PRN'    : {'dir':'r', 'rules':['VB.*']},
+            'IP-SUB'        : {'dir':'r', 'rules':['VB.*','BE.*','.*']},    #meira?
+            'IP-SUB-PRN'    : {'dir':'r', 'rules':['VB.*','BE.*']},
+            'IP-IMP'        : {'dir':'r', 'rules':['VB.*']},
+            'IP-SMC'        : {'dir':'r', 'rules':[]},
+            'IP-PPL'        : {'dir':'r', 'rules':[]},
+            'CP-THT'        : {'dir':'r', 'rules':['IP-SUB.*','.*']},
+            'CP-CAR'        : {'dir':'r', 'rules':['NP.*']},
+            'CP-CLF'        : {'dir':'r', 'rules':['IP-SUB.*']},
+            'CP-CMP'        : {'dir':'r', 'rules':['IP-SUB.*']},
+            'CP-DEG'        : {'dir':'r', 'rules':[]},
+            'CP-FRL'        : {'dir':'r', 'rules':[]},
+            'CP-REL'        : {'dir':'r', 'rules':['IP-SUB.*']},
+            'CP-QUE'        : {'dir':'r', 'rules':['IP-SUB.*']},
+            'CP-ADV'        : {'dir':'r', 'rules':['IP-SUB.*']},
+            'CP-EOP'        : {'dir':'r', 'rules':['IP-INF']},
+            'CP-TMC'        : {'dir':'r', 'rules':['IP-INF']},
+            'NP'            : {'dir':'r', 'rules':['NS-.', 'N-.', 'NPR-.']},
+            'NP-ADV'        : {'dir':'r', 'rules':['NP.*']},
+            'NP-CMP'        : {'dir':'r', 'rules':['N-.', 'NS-.', 'NPR-.']},
+            'NP-PRN'        : {'dir':'r', 'rules':['N-.', 'NS-.', 'NPR-.']},
+            'NP-SBJ'        : {'dir':'r', 'rules':['N-N', 'NS-N', 'NPR-N']},
+            'NP-OB1'        : {'dir':'r', 'rules':['N-A', 'NPR-A', 'NS-A', 'ONE+Q-A']},
+            'NP-OB2'        : {'dir':'r', 'rules':['NP.*', 'PRO-.', 'N-D', 'NS-D', 'NPR-.', 'CP-FRL', 'MAN-.']},    #MEIRA?
+            'NP-OB3'        : {'dir':'r', 'rules':['PRO-D', 'N-D', 'NS-D', 'NPR-D']},
+            'NP-PRD'        : {'dir':'r', 'rules':['NP.*', 'NPR-N']},
+            'NP-POS'        : {'dir':'r', 'rules':['N.*', 'PRO-.']},
+            'NP-COM'        : {'dir':'r', 'rules':[]},  #bara spor, hafa með?
+            'NP-ADT'        : {'dir':'r', 'rules':['N-.', 'NS-.', 'NPR-.']},
+            'NP-TMP'        : {'dir':'r', 'rules':['N-.', 'NS-.', 'NPR-.']},
+            'NP-MSR'        : {'dir':'r', 'rules':['NS-.', 'N-.']},
+            'ADJP'          : {'dir':'r', 'rules':['ADJ-N', 'ADJR-N', 'ADJS-N', 'ADJ.*', 'ADVR']},
+            'ADJP-SPR'      : {'dir':'r', 'rules':['ADJ-.', 'ADJS-N', 'ADJR-N']},
+            'PP'            : {'dir':'r', 'rules':['P']},
+            'PP-BY'         : {'dir':'r', 'rules':['P']},
+            'PP-PRN'        : {'dir':'r', 'rules':['P']},
+            'ADVP'          : {'dir':'r', 'rules':['ADV', 'WADV']},
+            'ADVP-DIR'      : {'dir':'r', 'rules':['ADV', 'WADV']},
+            'ADVP-LOC'      : {'dir':'r', 'rules':['ADV', 'WADV']},
+            'ADVP-TMP'      : {'dir':'r', 'rules':['ADV', 'WADV']},
+            'RP'            : {'dir':'r', 'rules':[]},  #tagg fyrir orð en ekki phrase type?
+            'CONJP'         : {'dir':'r', 'rules':['CONJ']},
+            'WNP'           : {'dir':'r', 'rules':['WPRO-.', 'PRO-.']}, #MEIRA?
+            'WPP'           : {'dir':'r', 'rules':['WNP', 'NP']}
+            }
 
     def _select_head(self, tree):
         '''
