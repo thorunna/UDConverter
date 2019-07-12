4db93c1e88a0ec0c9b20e9baa2268130d4e5d069
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2019-07-12 09:52:08 +0000

    minor fix

--- scripts/convert.py
+++ scripts/convert.py
@@ -18,38 +18,39 @@ icepahc = LazyCorpusLoader(
     r'.*\.psd', cat_pattern=r'.*(nar|rel|sci|bio|law)\-.*'
 )
 
-#if __name__ == '__main__':
-#fileids = icepahc.fileids() # leave uncommented for whole corpus use
-fileids = ['1985.sagan.nar-fic.psd'] # For debug use only
-c = Converter() # Creates instance of Converter class
-total_sents = 0
+if __name__ == '__main__':
+    #fileids = icepahc.fileids() # leave uncommented for whole corpus use
+    fileids = ['1883.voggur.nar-fic.psd'] # For debug use only
+    c = Converter() # Creates instance of Converter class
+    total_sents = 0
 
     #f = open('voggur_conllu.conllu', 'w+')
 
-''' Prints the dependency graph data in conllU format '''
-for fileid in fileids:
-    error_num = 0
-    start = time.time()
-    file_sents = 0
-    print('\nProcessing file: {0}...'.format(fileid))
-    for tree in icepahc.parsed_sents(fileid):
-        treeID = fileid + '_' + str(file_sents+1) + '_' + str(total_sents+1)
-        try:
-            dep = c.create_dependency_graph(str(tree))
-            dep_c = dep.to_conllU()
+    ''' Prints the dependency graph data in conllU format '''
+    for fileid in fileids:
+        error_num = 0
+        start = time.time()
+        file_sents = 0
+        print('\nProcessing file: {0}...'.format(fileid))
+        for tree in icepahc.parsed_sents(fileid):
+            treeID = fileid + '_' + str(file_sents+1) + '_' + str(total_sents+1)
+            try:
+                dep = c.create_dependency_graph(str(tree))
+                dep_c = dep.to_conllU()
                 #print(dep_c)
-                #f.write('# sent_id =', treeID)
-            print('# sent_id =', treeID)
+                #f.write('# sent_id =')
+                #f.write(treeID)
+                print('# sent_id =', treeID)
                 #f.write(dep.to_conllU())
-            print(dep.to_conllU())
-        except:
-            error_num += 1
-        file_sents += 1
-        total_sents += 1
-    end = time.time()
-    duration = '%.2f' % float(end - start)
-    print('Finished! Time elapsed: {0} seconds'.format(duration))
-    print('Number of sentences in file: {0}'.format(file_sents))
-    print('Number of failed sentences: {0}'.format(error_num))
+                print(dep.to_conllU())
+            except:
+                error_num += 1
+            file_sents += 1
+            total_sents += 1
+        end = time.time()
+        duration = '%.2f' % float(end - start)
+        print('Finished! Time elapsed: {0} seconds'.format(duration))
+        print('Number of sentences in file: {0}'.format(file_sents))
+        print('Number of failed sentences: {0}'.format(error_num))
 
     #f.close()
