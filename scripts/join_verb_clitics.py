c8bf7d3f5213747465f3a77a93d161d3a540dcd0
hinrikur <hih43@hi.is>
2019-08-01 15:33:21 +0000

    minor clitic fix

--- scripts/join_verb_clitics.py
+++ scripts/join_verb_clitics.py
@@ -21,19 +21,22 @@ Text preperation script for IcePaHC conllU-format output
         2	þú	þú	PRON	PRO-N	Case=Nom|Number=Plur|Gender=Neut	1	nsubj	_	_
 '''
 
-with open('homiliubok_test.conllu', 'r') as file:
-    outfile = open('out.homiliubok_test.conllu', 'w')
+in_path = str(sys.argv[1])
+out_path = in_path + '.tmp'
+
+with open(in_path, 'r') as file:
+    outfile = open(out_path, 'w')
     lines = file.readlines()
     indexes = reversed(range(len(lines)))
     for i in indexes:
         curr_line, prev_line = lines[i].split('\t'), lines[i-1].split('\t')
         if len(curr_line) == 1: continue
         # elif re.search('\$[a-zþæðöáéýúíó]', curr_line[1]):
-        elif re.search('\$[ðtd]u', curr_line[1]):
-            print(curr_line)
+        elif re.search('\$[ðtd]?[uú]', curr_line[1]):
+            # print(curr_line)
             joined_token = prev_line[1] + curr_line[1]
             if re.search(r'Mood=Imp', prev_line[5]):
-                if re.search(r'(s|[^Gg]er|[^á]t)\$', prev_line[1]):
+                if re.search(r'(s|[^Gg]er|[^áís]t)\$', prev_line[1]):
                     prev_line[1] = re.sub(r'\$', 't', prev_line[1])
                 if re.search(r'ð\$', prev_line[1]):
                     prev_line[1] = re.sub(r'ð\$', '', prev_line[1])
@@ -42,11 +45,11 @@ with open('homiliubok_test.conllu', 'r') as file:
                 # print(curr_line)
             elif re.search(r'[^iEeaæu]r\$', prev_line[1]):
                 prev_line[1] = re.sub(r'\$', 'ð', prev_line[1])
-            elif re.search(r'([Ee]r|s|al|[^í]t|un|il|rf)\$', prev_line[1]):
+            elif re.search(r'([Ee]r|s|al|[^ílntrs]t|un|il|rf)\$', prev_line[1]):
                 prev_line[1] = re.sub(r'\$', 't', prev_line[1])
             else:
                 prev_line[1] = re.sub(r'\$', '', prev_line[1])
-            curr_line[1] = re.sub(r'\$[tðd]u', 'þú', curr_line[1])
+            curr_line[1] = re.sub(r'\$[tðd]?[uú]', 'þú', curr_line[1])
             lines[i], lines[i-1] = '\t'.join(curr_line), '\t'.join(prev_line)
             joined_token = re.sub(r'\$\$', '', joined_token)
             joined_number = prev_line[0] + '-' + curr_line[0]
@@ -55,3 +58,6 @@ with open('homiliubok_test.conllu', 'r') as file:
     for line in lines:
         outfile.write(line)
     outfile.close()
+
+# os.remove(in_path)
+# os.rename(out_path, in_path)
