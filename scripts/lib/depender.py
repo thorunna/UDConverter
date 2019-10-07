e11135e57f1f25287cec162fa04786e3cda5fc21
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2019-10-07 22:30:47 +0000

    Update depender.py

--- scripts/lib/depender.py
+++ scripts/lib/depender.py
@@ -9,9 +9,10 @@ Based on earlier work by
 Part of UniTree project for IcePaHC
 '''
 
-from lib import features as f
+#from lib import features as f
 #from lib import DMII_data
-from lib.rules import head_rules, relation_NP, relation_IP, relation_CP
+from lib.rules import head_rules
+from lib import relations
 
 from nltk.tree import Tree
 from nltk.parse import DependencyGraph
@@ -200,8 +201,6 @@ class Converter():
         if '+' in head_tag:
             head_tag = re.sub('\w+\+', '', head_tag)
 
-        #todo use head_tag and more info about the constituency to better select the relation label
-
         if '-' in mod_tag:
             mod_tag, mod_func = mod_tag.split('-', 1) #todo, handle more than one function label
         else:
@@ -212,96 +211,7 @@ class Converter():
         else:
             head_func = None
 
-        if mod_tag in ['NP', 'NX', 'WNX']:   #TODO: hvað ef mod_tag er bara NP?
-            # -ADV, -CMP, -PRN, -SBJ, -OB1, -OB2, -OB3, -PRD, -POS, -COM, -ADT, -TMP, -MSR
-            return relation_NP.get(mod_func, 'rel')
-#            return relation_NP.get(mod_func, 'rel-'+mod_tag)
-        elif mod_tag == 'WNP':
-            return 'obj'
-        elif mod_tag in ['NS', 'N', 'NPRS'] and head_tag in ['NP', 'NX', 'QTP', 'ADJP', 'CONJP', 'NPR']:    #seinna no. í nafnlið fær 'conj' og er háð fyrra no.
-            return 'conj'
-        elif mod_tag == 'NPR' and head_tag == 'CONJP':
-            return 'conj'
-        elif mod_tag == 'NPR' and head_tag in ['NP', 'NX', 'QTP']:
-            return 'flat:name'
-        elif mod_tag == 'ES':
-            return 'expl'   #expletive
-        elif mod_tag in ['PRO', 'WPRO']:
-            return 'nmod?'
-#        elif mod_tag == 'PRO' and head_tag == 'NP' and head_func == 'PRN':  #TODO: skoða betur, hliðstæð NPR sem eru bæði dobj?
-#            return 'obj'
-        elif mod_tag in ['D', 'WD', 'ONE', 'ONES', 'OTHER', 'OTHERS', 'SUCH']:
-            return 'det'
-        elif mod_tag[:3] == 'ADJ' or mod_tag[:4] == 'WADJ' or mod_tag in ['Q', 'QR', 'QS', 'WQP']:
-            # -SPR (secondary predicate)
-            return 'amod'
-        elif mod_tag in ['PP', 'WPP', 'PX']:
-            # -BY, -PRN
-            return 'obl'        #NP sem er haus PP fær obl nominal  #TODO: haus CP-ADV (sem er PP) á að vera merktur advcl
-        elif mod_tag == 'P':
-            return 'case'
-        elif mod_tag[:3] == 'ADV' or mod_tag in ['NEG', 'FP', 'QP', 'ALSO', 'WADV', 'WADVP']:    #FP = focus particles  #QP = quantifier phrase - ATH.
-            # -DIR, -LOC, -TP
-            return 'advmod'
-        elif mod_tag == 'NS' and head_tag == 'ADVP' and head_func == 'TMP':     #ath. virkar fyrir eitt dæmi, of greedy?
-            return 'conj'
-        elif mod_tag in ['RP', 'RPX']:
-            return 'compound:prt'
-        elif mod_tag == 'IP' and mod_func == 'SUB' and head_tag == 'CP' and head_func == 'FRL':
-            return 'acl:relcl'
-        elif mod_tag in ['IP', 'VP']:
-            return relation_IP.get(mod_func, 'rel')
-#            return relation_IP.get(mod_func, 'rel-'+mod_tag)
-        elif mod_tag[:2] == 'VB' and head_tag == 'CP':
-            return 'ccomp'
-        elif mod_tag in ['VAN', 'DAN', 'HAN', 'BAN']:
-            return 'aux:pass'
-        elif mod_tag in ['VBN', 'DON', 'HVN', 'RDN']:   #ath. VBN getur verið rót
-            return '?'
-        elif mod_tag[:2] in ['VB', 'DO', 'HV', 'RD', 'MD']: #todo
-            return 'aux'
-        elif mod_tag[:2] == 'BE' or mod_tag == 'BAN':  #copular, TODO: ekki alltaf copular
-            return 'cop'
-        elif mod_tag == 'VAG':
-            return 'amod?'
-        elif mod_tag == 'RRC':
-            return 'acl:relcl?'
-        elif mod_tag == 'CONJ':
-            return 'cc'
-        elif mod_tag in ['CONJP', 'N'] and head_tag in ['NP', 'N', 'PP']:      #N: tvö N í einum NP tengd með CONJ
-            return 'conj'
-        elif mod_tag == 'CONJP' and head_tag == 'IP':
-            return relation_IP.get(head_func, 'rel')
-#            return relation_IP.get(head_func, 'rel-'+mod_tag+head_tag+head_func)
-        elif mod_tag == 'CONJP':
-            return 'conj'
-        elif mod_tag == 'CP':
-            return relation_CP.get(mod_func, 'rel')
-#            return relation_CP.get(mod_func, 'rel-'+mod_tag)
-        elif mod_tag in ['C', 'CP', 'TO', 'WQ']:  #infinitival marker with marker relation
-            return 'mark'
-        elif mod_tag in ['NUM', 'NUMP']:
-            return 'nummod'
-        elif mod_tag == 'FRAG':
-            return 'xcomp'
-        elif mod_tag in string.punctuation or mod_tag == 'LB':
-            return 'punct'
-        elif mod_tag in ['INTJ', 'INTJP'] or head_tag == 'INTJP':
-            return 'discourse'
-        elif mod_tag in ['FOREIGN', 'FW', 'ENGLISH', 'LATIN'] or head_tag in ['FOREIGN', 'FW', 'ENGLISH', 'LATIN']:
-            return 'foreign'
-        elif mod_tag in ['XXX', 'XP', 'X', 'QTP', 'REP', 'FS', 'LS', 'META', 'REF']:      #XXX = annotator unsure of parse, LS = list marker
-            return 'dep'    #unspecified dependency
-        elif head_tag in ['META', 'CODE', 'REF', 'FRAG']:
-            return 'dep'
-        elif mod_tag in ['N', 'NS', 'NPR', 'NPRS']:
-            return 'rel'
-
-        return 'rel-'+mod_tag
-#        if mod_func:
-#            return 'rel-'+mod_tag+'+'+mod_func+'+'+head_tag
-#        else:
-#            return 'rel-'+mod_tag+'+'+head_tag
+        return relations.determine_relations(mod_tag, mod_func, head_tag, head_func)
 
     def create_dependency_graph(self, tree):
         """Create a dependency graph from a phrase structure tree."""
@@ -356,20 +266,21 @@ class Converter():
                 leaf = token_lemma, tag
                 XPOS = tag
                 # Feature Classes called here
-                leaf = f.Word(leaf).getinfo()
-                UPOS = leaf.UD_tag
-                FEATS = leaf.features.featString()
-                self.dg.add_node({'address': nr,
-                                  'word': FORM,
-                                  'lemma': LEMMA,
-                                  #'ctag': UPOS, # upostag
-                                  'ctag': None, # upostag
-                                  'tag': XPOS,   # xpostag
-                                  #'feats': FEATS,
-                                  'feats': None,
-                                  'deps': defaultdict(list),
-                                  'rel': None})
-                nr += 1
+                #leaf = f.Word(leaf).getinfo()
+                #UPOS = leaf.UD_tag
+                #FEATS = leaf.features.featString()
+                if FORM != None:
+                    self.dg.add_node({'address': nr,
+                                    'word': FORM,
+                                    'lemma': LEMMA,
+                                    #'ctag': UPOS, # upostag
+                                    'ctag': '_', # upostag
+                                    'tag': XPOS,   # xpostag
+                                    #'feats': FEATS,
+                                    'feats': '_',
+                                    'deps': defaultdict(list),
+                                    'rel': '_'})
+                    nr += 1
 
         # go through the constituencies (bottom up) and find their heads
         const.sort(key=lambda x: len(x), reverse=True)
@@ -391,7 +302,18 @@ class Converter():
 #                    self.dg.get_by_address(mod_nr).update({'head': 0, 'rel': 'root'})
 #                    self.dg.root = self.dg.get_by_address(mod_nr)
                 if child:
-                    if head_nr == mod_nr and re.match( "IP-MAT.*|CP.*|INTJP|FRAG", head_tag):  #todo root phrase types from config
+                    #if child[0] == '0' or '*' in child[0] or '{' in child[0] or '<' in child[0] or mod_tag == 'CODE':
+                    #    continue
+                    #else:
+                    #    if head_nr == mod_nr and re.match("IP-MAT.*|INTJP|FRAG|CP-QUE-SPE|IP-IMP-SPE|QTP", head_tag):  #todo root phrase types from config
+                    #        self.dg.get_by_address(mod_nr).update({'head': 0, 'rel': 'root'})  #todo copula not a head
+                    #        self.dg.root = self.dg.get_by_address(mod_nr)
+                    #    else:
+                    #        self.dg.get_by_address(mod_nr).update({'head': head_nr, 'rel': self._relation(mod_tag, head_tag)})
+                    #    if head_nr != mod_nr:
+                    #        self.dg.add_arc(head_nr, mod_nr)
+                    #if head_nr == mod_nr and re.match("IP-MAT.*|CP.*|INTJP|FRAG", head_tag):  #todo root phrase types from config
+                    if head_nr == mod_nr and re.match("IP-MAT.*|INTJP|FRAG|CP-QUE-SPE|IP-IMP-SPE|QTP", head_tag):  #todo root phrase types from config
                         self.dg.get_by_address(mod_nr).update({'head': 0, 'rel': 'root'})  #todo copula not a head
                         self.dg.root = self.dg.get_by_address(mod_nr)
                     elif child[0] == '0' or '*' in child[0] or '{' in child[0] or '<' in child[0] or mod_tag == 'CODE':
