452fc14a9cf84cef4766dfbd5b4e449bd0259e5d
hinrikur <hih43@hi.is>
2020-03-03 00:18:39 +0000

    conversion output set to testing folder

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,57 +4,65 @@ from nltk.corpus.util import LazyCorpusLoader
 from nltk.corpus.reader import CategorizedBracketParseCorpusReader
 from nltk.data import path
 from nltk.tree import *
-from collections import defaultdict
 import time
 import re
-import string
+import os
 
-path.extend(['./testing/'])
+path.extend(['../testing/'])
 
 icepahc = LazyCorpusLoader(
-    'icepahc-v0.9/psd/', CategorizedBracketParseCorpusReader,
+    'icecorpus/psd/', CategorizedBracketParseCorpusReader,
     r'.*\.psd', cat_pattern=r'.*(nar|rel|sci|bio|law)\-.*'
 )
 
 if __name__ == '__main__':
-    #fileids = icepahc.fileids() # leave uncommented for whole corpus use
-    fileids = ['2008.ofsi.nar-sag.psd'] # For debug use only
+    fileids = icepahc.fileids() # leave uncommented for whole corpus use
+    # fileids = ['2008.ofsi.nar-sag.psd'] # For debug use only
     c = Converter() # Creates instance of Converter class
     total_sents = 0
     file_num = 1
 
-    f = open('ofsi_conllu.conllu', 'w+')
+    outputDir = '../testing/CoNLLU_output/'
+    if not os.path.isdir(outputDir):
+        os.mkdir(outputDir)
+
+    # f = open('ofsi_conllu.conllu', 'w+')
 
     ''' Prints the dependency graph data in conllU format '''
     for fileid in fileids:
+
+        outFileName = re.sub(r'\.psd', '.conllu', fileid)
+        outFilePath = outputDir + outFileName
+        outFile = open(outFilePath, 'w+')
+
         error_num = 0
         start = time.time()
         file_sents = 0
-        error_file = []
-        #print('\nProcessing file: {0}...'.format(fileid))
+        # print('\nProcessing file: {0}...'.format(fileid))
+
         for tree in icepahc.parsed_sents(fileid):
             treeID = fileid + '_' + str(file_sents+1) + '_' + str(total_sents+1)
             try:
                 dep = c.create_dependency_graph(str(tree))
-                #dep_c = dep.to_conllU()
-                #print(dep_c)
-                f.write('# sent_id =')
-                f.write(treeID)
-                f.write('\n')
-                print('# sent_id =', treeID)
-                f.write(dep.to_conllU())
-                print(dep.to_conllU())
-            except:
+                dep_c = dep.to_conllU()
+                outFile.write('# sent_id = ')
+                outFile.write(treeID)
+                outFile.write('\n')
+                outFile.write(dep_c)
+                # print('# sent_id =', treeID)
+                # print(dep_c)
+            except Exception as ex:
+                print('ERROR', '# sent_id =', treeID)
+                print(tree)
+                # print('Failure - {0}. Arguments:\n{1!r}'.format(type(ex).__name__, ex.args))
+                raise
                 error_num += 1
-                error_file.append(treeID)
             file_sents += 1
             total_sents += 1
         end = time.time()
         duration = '%.2f' % float(end - start)
-        print('Finished! Time elapsed: {0} seconds'.format(duration))
-        print('Number of sentences in file: {0}'.format(file_sents))
-        print('Number of failed sentences: {0}'.format(error_num))
-        print(error_file)
-        #print('\t'.join([str(i) for i in [file_num, fileid, error_num]]))
+        # if error_num > 0:
+        print('\t'.join([str(i) for i in [file_num, fileid,file_sents, error_num, str(duration)+' sec']]))
+        file_num += 1
 
-    f.close()
+    # f.close()
