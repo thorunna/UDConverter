0d37baaf758bd1a09068111487e0ac179125a73c
hinrikur <hih43@hi.is>
2020-04-02 19:37:40 +0200

    Debug: updated root counting script for ccomp use

--- scripts/extractors/root_not_1.py
+++ scripts/extractors/root_not_1.py
@@ -1,15 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""06.03.20
+
+Hinrik Hafsteinsson (hih43@hi.is)
+Þórunn Arnardóttir (tha86@hi.is)
+
+Debugging script that counts roots in CoNLL-U format sentences.
+In UD each sentence should have exactly 1 root.
+Part of UniTree project for IcePaHC
+
+"""
+
 import pyconll
 import pyconll.util
 import sys
 import os
-
 import argparse
 
-# script that reads conllu file as first argument (path) and prints sentences
-# that have != 1 root (most likely 0 or 2 roots)
 
 def get_root_errors(in_file, no_roots_file, many_roots_file):
-    """
+    """10.03.20
     Short summary.
 
     Args:
@@ -50,30 +60,83 @@ def get_root_errors(in_file, no_roots_file, many_roots_file):
 
     # print('Sents. with None (parse error):', parse_errors)
 
+def get_ccomp_xcomp_errors(in_file, ccomp_xcomp_file):
+    """26.03.20
+    Short summary.
+
+    Args:
+        in_file (string): Description of parameter `in_file`.
+        ccomp_xcomp_file (string): Description of parameter `no_roots_file`.
+
+    Returns:
+        None
+
+    """
+    ccomp_count = 0
+    xcomp_count = 0
+    error_count = 0
+    error_sents = 0
+    parse_errors = 0
+
+    for sentence in in_file:
+        for token in sentence:
+            if token.deprel == 'ccomp/xcomp':
+                error_count += 1
+            elif token.deprel == 'ccomp':
+                ccomp_count += 1
+            elif token.deprel == 'xcomp':
+                xcomp_count += 1
+    return (ccomp_count, xcomp_count, error_count)
+
+
+    # print('Sents. with None (parse error):', parse_errors)
+
+
 def main():
 
     parser = argparse.ArgumentParser(description='Count root errors in CoNLL-U file corpus')
     parser.add_argument('--input', '-i', type=str, required=True,
                         help='path to input CoNLL-U (.conllu) directory')
+    # parser.add_argument('--output', '-o', type=str, required=False,
+    #                     help='path to output directory')
     parser.add_argument('--verbose', '-v', action='store_true',
                         help='verbose flag')
+
+    error_types = parser.add_mutually_exclusive_group(required=True)
+    error_types.add_argument('--roots', '-r',
+                        help='check for number of roots in sentences', action='store_true')
+    error_types.add_argument('--ccomp', '-c',
+                        help='check for number of ccomp/xcomp in files', action='store_true')
+
     args = parser.parse_args()
 
-    f = open('root_check/no_root_allt.txt', 'w+')
-    f2 = open('root_check/too_many_roots_allt.txt', 'w+')
+
     conllu_path = args.input
     for conllu_file in os.listdir(conllu_path):
         train = pyconll.load_from_file(os.path.join(conllu_path, conllu_file))
-        if args.verbose:
-            print('Current file:', conllu_file)
-            no_roots, many_roots = get_root_errors(train, f, f2)
-            print('Sents. with no roots:', no_roots)
-            print('Sents. with many roots:', many_roots)
-        else:
-            no_roots, many_roots = get_root_errors(train, f, f2)
-            print(f'{no_roots}\t{many_roots}\t{conllu_file}')
-    f.close()
-    f2.close()
+        if args.roots:
+            f = open('root_check/no_root_allt.conllu', 'w+')
+            f2 = open('root_check/too_many_roots_allt.conllu', 'w+')
+            if args.verbose:
+                print('Current file:', conllu_file)
+                no_roots, many_roots = get_root_errors(train, f, f2)
+                print('Sents. with no roots:', no_roots)
+                print('Sents. with many roots:', many_roots)
+            else:
+                no_roots, many_roots = get_root_errors(train, f, f2)
+                print(f'{no_roots}\t{many_roots}\t{conllu_file}')
+            f.close()
+            f2.close()
+        if args.ccomp:
+            f = open('root_check/ccomp_xcomp_allt.conllu', 'w+')
+            if args.verbose:
+                print('Current file:', conllu_file)
+                ccomp_xcomp_erros = get_ccomp_xcomp_errors(train, f)
+                print('No. of ccomp/xcomp relations:', ccomp_xcomp_erros)
+            else:
+                ccomp, xcomp, ccomp_xcomp = get_ccomp_xcomp_errors(train, f)
+                print(f'{ccomp}\t{xcomp}\t{ccomp_xcomp}\t{conllu_file}')
+            f.close()
 
 
 if __name__ == '__main__':
