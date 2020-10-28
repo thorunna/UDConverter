ec9683a859247e5b0d621a81780a9beb3740c907
hinrikur <hih43@hi.is>
2020-10-28 13:59:23 +0000

    space before period no longer removed by default

--- scripts/lib/depender.py
+++ scripts/lib/depender.py
@@ -276,7 +276,7 @@ class UniversalDependencyGraph(DependencyGraph):
         text = re.sub(r' \$', ' ', text)
         text = re.sub(r'\$ ', ' ', text)
         text = re.sub(r' $', '', text)
-        text = re.sub(r' \.', '.', text)
+        # text = re.sub(r' \.', '.', text)
         text = re.sub(r' ,', ',', text)
         return '# text = ' + text
 
