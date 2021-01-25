6bf4eb5ba1d8892dcd5867d522fb1c22856fe210
hinrikur <hih43@hi.is>
2021-01-25 22:56:14 +0000

    added script for counting words in files

---
+++ Manual_correction/count_dir_words.sh
@@ -0,0 +1,11 @@
+
+# example usage:
+# count_dir_words.sh additions2019/althingi/bo/2013/
+
+directory=$1
+
+for file in $directory/*; do
+  word_num=$(sed '/#/d' $file | sed '/^$/d' | wc -l)
+  filename=${file##*/}
+  echo -e "${filename%.conllu}\t$word_num"
+done
